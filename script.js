67ee73b749f3a2cf77421ec8d3500fe88d8fc0d6
Bruno Ferreira Lopes da Costa <142623420+bferreiradacosta@users.noreply.github.com>
2023-08-20 12:31:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -148,14 +148,6 @@ const sizes = [
     [1, max, max, max, 3, max]	// Number of sentences on the first list in each box
 ];
 
-// NEW STUFF
-
-async function fetchText() {
-    let response = await fetch('/sentences/');
-    let data = await response.text();
-    console.log(data);
-}
-
 // ------------------------------
 // DO NOT CHANGE BELOW THIS LINE!
 // ------------------------------
@@ -259,6 +251,4 @@ window.onload = function() {
         box.forEach(shuffleList);
         box.forEach((_, pos) => updateBox(pos, index, 0));
     });
-
-fetchText();
 };
